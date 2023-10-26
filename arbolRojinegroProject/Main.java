68daaf4d59723aefbf5262479a39cf5a90bf5fac
alejandrojsilvestre <alejandro.j.silvestre@gmail.com>
2023-10-25 23:13:23 -0300

    Agrego estructura de arbol

---
+++ arbolRojinegroProject/Main.java
@@ -0,0 +1,11 @@
+package arbolRojinegroProject;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Arbol arbol = new Arbol();
+
+        arbol.insertar(20);
+    }
+    
+}
---
+++ arbolRojinegroProject/utils/Nodo.java
@@ -0,0 +1,9 @@
+package arbolRojinegroProject.utils;
+
+public class Nodo {
+    public int data;
+    public Nodo father;
+    public Nodo left;
+    public Nodo right;
+    public int color; // 0 para negro, 1 para rojo
+}
